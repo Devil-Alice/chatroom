0845a798d08629358bb7011e91fd0d6a2dc4926f
Devil-Alice <alice.devil@outlook.com>
2025-03-10 18:40:39 +0800

    添加获验证码的proto文件；添加获取验证码的grpc客户端代码

---
+++ src/verify_grpc_client.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <grpcpp/grpcpp.h>
+#include "global.h"
+#include "my_message.pb.h"
+#include "my_message.grpc.pb.h"
+
+// 引入作用域
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+using my_message::GetVerifyRequest;
+using my_message::GetVerifyResponse;
+using my_message::VerifyService;
+
+class VerifyGrpcClient : public Singleton<VerifyGrpcClient>
+{
+    friend class Singleton<VerifyGrpcClient>;
+
+private:
+    // 在 gRPC 中，stub（存根）是 客户端用于调用 RPC 方法的代理对象，它封装了网络通信的细节，使得客户端可以像调用本地函数一样调用远程服务
+    std::unique_ptr<VerifyService::Stub> stub_;
+
+private:
+    VerifyGrpcClient()
+    {
+        // 初始化channel，初始化stub
+        std::shared_ptr<Channel> channel = grpc::CreateChannel("127.0.0.1:50053", grpc::InsecureChannelCredentials());
+        stub_ = VerifyService::NewStub(channel);
+    }
+
+public:
+    // ~VerifyGrpcClient();
+    GetVerifyResponse get_rerify_code(std::string phone)
+    {
+        ClientContext context;
+        GetVerifyRequest req;
+        GetVerifyResponse rsp;
+
+        req.set_phone(phone);
+        Status status = stub_->get_verify_code(&context, req, &rsp);
+        if (status.ok())
+            return rsp;
+        else
+            rsp.set_error(MY_ERROR_CODE::RPC_RAILED);
+
+        return rsp;
+    }
+};

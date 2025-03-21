a1a76e9368a84774e32e6c831f6b8cf16e0441ff
Devil-Alice <alice.devil@outlook.com>
2025-03-21 16:44:18 +0800

    优化JsonObject

--- server/src/json_object.h
+++ server/src/json_object.h
@@ -1,11 +1,25 @@
 #pragma once
 #include <string>
+#include <jsoncpp/json/json.h>
 
 class JsonObject
 {
     using string = std::string;
 
 public:
-    virtual string to_json_string() = 0;
+    virtual ~JsonObject(){};
+    static Json::Value parse_json_string(string json_string)
+    {
+        Json::Reader reader;
+        Json::Value root;
+
+        bool success = reader.parse(json_string, root);
+        if (!success || !root.isArray())
+            throw std::runtime_error("JsonObject parse_json_string error");
+        return root;
+    }
+
+    virtual Json::Value to_json() = 0;
+    virtual string to_json_string() { return to_json().toStyledString(); }
     virtual void from_json_string(string json_string) = 0;
 };

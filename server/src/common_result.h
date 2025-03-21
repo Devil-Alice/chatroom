4723652ada9d05e392dc137f7364bfdb21e227f9
Devil-Alice <alice.devil@outlook.com>
2025-03-21 18:33:15 +0800

    优化CommonResult，添加set方法

--- server/src/common_result.h
+++ server/src/common_result.h
@@ -10,19 +10,29 @@ class CommonResult
     using string = std::string;
 
 private:
-public:
-    CommonResult(int status, string message, std::shared_ptr<JsonObject> data = nullptr)
-        : status_(status), message_(message), data_(data) {}
-    ~CommonResult(){};
     // 结果状态
     int status_;
     string message_;
     // 响应的数据
     std::shared_ptr<JsonObject> data_;
 
+public:
+    CommonResult() {};
+    CommonResult(int status, string message, std::shared_ptr<JsonObject> data = nullptr)
+        : status_(status), message_(message), data_(data) {}
+    ~CommonResult() {};
+
+    // 方便链式调用
+    CommonResult &set(int status, string message, std::shared_ptr<JsonObject> data = nullptr)
+    {
+        status_ = status;
+        message_ = message;
+        data_ = data;
+        return *this;
+    }
+
     string to_json_string()
     {
-        Json::Reader reader;
         Json::Value root;
 
         root["status"] = status_;

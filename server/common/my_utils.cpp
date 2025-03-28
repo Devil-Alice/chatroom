0d639d1e5d6b0f4b57c1841d4319e39e4d25d076
Devil-Alice <alice.devil@outlook.com>
2025-03-28 15:27:27 +0800

    服务端工具类添加生成uuid的方法

--- server/common/my_utils.cpp
+++ server/common/my_utils.cpp
@@ -2,6 +2,10 @@
 
 namespace my_utils
 {
-
+    string generate_uuid()
+    {
+        boost::uuids::random_generator gen;
+        return boost::uuids::to_string(gen());
+    }
 
 }
--- server/common/my_utils.h
+++ server/common/my_utils.h
@@ -1,8 +1,12 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
 
 namespace my_utils
 {
-
+    using namespace std;
+    string generate_uuid();
 }
